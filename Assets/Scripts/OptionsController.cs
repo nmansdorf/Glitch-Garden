7efae97e786eaa21c4d7c257daa68fd567fa6e15
Nick Mansdorf <nickm@spaceapegames.com>
2018-04-23 17:17:59 +0100

    glitch garden

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+
+	public Slider VolumeSlider, DifficultySlider;
+	public Text DifficultyText;
+
+	private int difficulty;
+	private float volume;
+	private string difficultyString;
+	private MusicManager musicManager;
+	
+	public float DEFAULT_VOLUME = 0.8f;
+	public int DEFAULT_DIFFICULTY = 1;
+
+	void Start()
+	{
+		musicManager = FindObjectOfType<MusicManager>();
+		VolumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+		DifficultySlider.value = PlayerPrefsManager.GetDifficulty();
+		SetDifficultyString((int)DifficultySlider.value);
+
+	}
+
+	void Update()
+	{
+		musicManager.ChangeVolume(VolumeSlider.value);
+		SetDifficultyString((int)DifficultySlider.value);
+	}
+
+	public void SaveAndExit()
+	{
+		PlayerPrefsManager.SetMasterVolume(VolumeSlider.value);
+		PlayerPrefsManager.SetDifficulty((int)DifficultySlider.value);
+		LevelManager levelManager = FindObjectOfType<LevelManager>();
+		levelManager.LoadLevel("Scenes/Main");
+	}
+
+	void SetDifficultyString(int difficulty)
+	{
+		switch (difficulty)
+		{
+			case 0:
+				difficultyString = "Easy";
+				break;
+			case 1:
+				difficultyString = "Normal";
+				break;
+			case 2:
+				difficultyString = "Hard";
+				break;
+			case 3:
+				difficultyString = "Very Hard";
+				break;
+		}
+
+		DifficultyText.text = difficultyString;
+	}
+
+	public void SetDefaults()
+	{
+		PlayerPrefsManager.SetMasterVolume(DEFAULT_VOLUME);
+		VolumeSlider.value = DEFAULT_VOLUME;
+		
+		PlayerPrefsManager.SetDifficulty(DEFAULT_DIFFICULTY);
+		DifficultySlider.value = DEFAULT_DIFFICULTY;
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+
+	private const string MASTER_VOLUME_KEY = "master_volume";
+	private const string DIFFICULTY_KEY = "difficulty";
+	private const string LEVEL_KEY = "level_unlocked_";
+	public static int MaxDifficulty = 3;
+
+
+	public static void SetMasterVolume(float volume)
+	{
+		if (volume >= 0f && volume <= 1f)
+		{
+			PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+		}
+		else
+		{
+			Debug.LogError("Master volume" + volume + " out of range");
+		}
+	}
+
+	public static float GetMasterVolume()
+	{
+		return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+	}
+	
+	public static void SetDifficulty(int difficulty)
+	{
+		if (difficulty >= 0 && difficulty < MaxDifficulty)
+		{
+			PlayerPrefs.SetInt(DIFFICULTY_KEY, difficulty);
+		}
+		else
+		{
+			Debug.LogError("Difficulty "+ difficulty +" out of range");
+		}
+	}
+
+	public static int GetDifficulty()
+	{
+		return PlayerPrefs.GetInt(DIFFICULTY_KEY);
+	}
+	
+	public static void UnlockLevel(int level)
+	{
+		if (level > 0 && level < SceneManager.sceneCount - 1)
+		{
+			PlayerPrefs.SetInt(LEVEL_KEY + level, 1); //Set 1 for true
+		}
+		else
+		{
+			Debug.LogError("Level unlock " + level + " out of range");
+		}
+	}
+
+	public static bool IsLevelUnlocked(int level)
+	{
+		if (PlayerPrefs.GetInt(LEVEL_KEY + level) == 1)
+		{
+			return true;
+		}
+		else 
+		{
+			Debug.LogError("Level unlock " + level + "not in build order" );
+			return false;
+		}
+	}
+}
